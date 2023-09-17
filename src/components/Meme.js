1fbe93773bc5dc69edca8f502ee2f56838efdd9b
PANKAJ RAJPUT <thakurrajputpankaj0@gmail.com>
2023-09-17 12:37:32 -0400

    states - part 2

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,12 +2,30 @@ import React from "react";
 import data from '../memesData'
 
 export default function Meme () {
-    const [memeImage , setMemeImage] = React.useState("")
+    //const [memeImage , setMemeImage] = React.useState("")
+    const [meme, setMeme] = React.useState({
+        topText : "",
+        bottomText : "",
+        randomImage : "http://i.imgflip.com/1bij.jpg"
+    })
+    const [allMemeImages, setAllMemeImages] = React.useState(data)
+
     function handlegetImages(){
         let memesArray = data.data.memes;
         let index = Math.floor(Math.random()* memesArray.length)
         let url = memesArray[index].url;
-        setMemeImage(url)
+        //setMemeImage(url)
+    }
+    function getMemeImage(){
+        const memesArray = allMemeImages.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme =>{
+            return {
+                ...prevMeme,
+                randomImage : url
+            }
+        })
     }
     return (
         <main>
@@ -23,11 +41,11 @@ export default function Meme () {
                     className="form--input"
                 />
                 <button 
-                    className="form--button" onClick={handlegetImages}
+                    className="form--button" onClick={getMemeImage}
                 >
                     Get a new meme image / Getting back to learning React
                 </button>
-                <img src={memeImage} className="meme--image"/>
+                <img src={meme.randomImage} className="meme--image"/>
             </div>
         </main>
     )
