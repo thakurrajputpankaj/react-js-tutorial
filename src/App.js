62c49c1e18a87d6b33bfcdbfbcf657c51451bfa8
PANKAJ RAJPUT <thakurrajputpankaj0@gmail.com>
2023-09-28 13:50:18 -0400

    hold-dice complete

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import {nanoid} from "nanoid"
 
 export default function App() {
 
+    function generateNewDie() {
+        return {
+            value: Math.ceil(Math.random() * 6),
+            isHeld: false,
+            id: nanoid()
+        }
+    }
+
     function allNewDice() {
         const min = 1;
         const max = 6;
@@ -25,18 +33,25 @@ export default function App() {
 
     const diceElements = dice.map(die => <Dice key={die.id} value={die.value} isHeld={die.isHeld} holdDice={()=> holdDice(die.id)}/>)
 
-    function rollDice(){
-        setDice(allNewDice())
-    }
-
-    function holdDice(id){
+    function rollDice() {
         setDice(oldDice => oldDice.map(die => {
-            return die.id === id ? {...die, isHeld : !die.isHeld} : die
+            return die.isHeld ? 
+                die :
+                generateNewDie()
         }))
     }
-
+    
+    function holdDice(id) {
+        setDice(oldDice => oldDice.map(die => {
+            return die.id === id ? 
+                {...die, isHeld: !die.isHeld} :
+                die
+        }))
+    }
     return (
         <main>
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="dices">
                 {diceElements}
             </div>
